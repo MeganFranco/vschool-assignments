a8a2ede76780eac553adc148fd3c1eb4dd25bbd8
champagnecurves <francomegan@gmail.com>
2016-05-03 15:27:09 -0600

    May third updates

---
+++ exercises/social-media-exp.js
@@ -0,0 +1,150 @@
+var theBlackSwans = 
+[    
+   
+    {
+        name: "Bella",
+        position: "Troop Leader",
+        age: 36,
+        yearsDancing: 30,
+        favoriteStyle: "Ballet",
+        otherTroops: ["Swing Dancers Unanonymous", "Jezebels of Jazz"
+            "Tap Tap Tappers", "Hip Hop Ladies", "Polka Party"],
+        fans: [
+            {
+             name: "George",
+             hometown: "Toledo",
+             ticketsPurchased: 6,
+             frequentFriendClub: function () {
+                 if (ticketsPurchsed > 5) {
+                     return this.name + " is in Friends Club and eligible for discounts";
+                     var friend true
+                 } else return this.name + "is not eligilble for discounts";
+                 var friend = true
+                } 
+             },
+            
+             {  
+                name: "Sharron",
+                hometown: "Oakland",
+                ticketsPurchased: 9,
+                frequentFriendClub: function () {
+                     if (ticketsPurchsed > 5) {
+                         return this.name + " is in Friends Club and eligible for discounts";
+                         var friend true
+                     } else return this.name + " is not eligilble for discounts";
+                     var friend = false
+                    }
+               },
+        {
+            name: "Joe",
+            hometown: "Princeton",
+            ticketsPurchased: 5,
+            frequentFriendClub: function () {
+                if (ticketsPurchsed > 5) {
+                    return this.name + " is in Friends Club and eligible for discounts";
+                    var friend = true
+                } else return this.name + " is not eligilble for discounts";
+                var friend = false
+            }
+        },
+            
+            ],
+    },
+    },
+        
+    {
+        name: "Samantha",
+        position: "Senior Dancer",
+        age: 27,
+        yearsDancing: 19,
+        favoriteStyle: "Jazz",
+        otherTroops: ["Jezebels of Jazz"
+            "Hip Hop Ladies", "Polka Party"],
+        fans: [{
+            name: "George",
+            hometown: "Toledo",
+            ticketsPurchased: 6,
+            frequentFriendClub: function () {
+                if (ticketsPurchsed > 5) {
+                    return this.name + " is in Friends Club and eligible for discounts";
+                    var friend true
+                } else return this.name + "is not eligilble for discounts";
+                var friend = true
+                }
+            }, 
+            {
+            name: "Perla",
+            hometown: "Pittsburg",
+            ticketsPurchased: 4,
+            frequentFriendClub: function () {
+                if (ticketsPurchsed > 5) {
+                    return this.name + " is in Friends Club and eligible for discounts";
+                    var friend = true
+                } else return this.name + " is not eligilble for discounts";
+                var friend = false
+            }, 
+               fan5],
+    },
+               
+    {
+        name: "Ellen",
+        position: "junior dancer",
+        age: 23,
+        yearsDancing: 16,
+        favoriteStyle: "hip-hop",
+        otherTroops: ["Tap Tap Tappers", "Hip Hop Ladies"],
+        fans: [{
+            name: "Sharron",
+            hometown: "Oakland",
+            ticketsPurchased: 9,
+            frequentFriendClub: function () {
+                if (ticketsPurchsed > 5) {
+                    return this.name + " is in Friends Club and eligible for discounts";
+                    var friend true
+                } else return this.name + " is not eligilble for discounts";
+                var friend = false
+                }
+            }, 
+            {
+            name: "Joe",
+            hometown: "Princeton",
+            ticketsPurchased: 5,
+            frequentFriendClub: function () {
+                if (ticketsPurchsed > 5) {
+                    return this.name + " is in Friends Club and eligible for discounts";
+                    var friend = true
+                } else return this.name + " is not eligilble for discounts";
+                var friend = false
+                }
+            }
+              ]
+        
+    },
+    
+
+            }
+        }
+]
+               
+
+    
+
+
+
+//
+//frequentFriendClub: function ()
+//
+//////membermenu
+////    name:
+////    position:
+////    age:
+////    yearsDancing:
+////    favoriteStyle:
+////    otherTroops:
+////    fans:
+////
+//////fan menu
+////name:
+////hometown:
+////ticketsPurchased:
+//////method here if they make it into the club?
