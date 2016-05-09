c293d87dcb7c362570da5879b308992dd60487e7
Megan Franco <francomegan@gmail.com>
2016-05-09 16:15:19 -0600

    re-adding everything

---
+++ exercises/Day6JS/DJ-JS.js
@@ -0,0 +1,43 @@
+function blueMe() {
+    document.getElementById("mySquare").style.backgroundColor= "blue"; 
+}
+
+function greenMe() {
+    document.getElementById("mySquare").style.backgroundColor= "green" 
+}
+
+function redMe() {
+    document.getElementById("mySquare").style.backgroundColor= "red"; 
+}
+
+function yellowMe(){
+    document.getElementById("mySquare").style.backgroundColor= "yellow"; 
+}
+
+function orangeMe() {
+    document.getElementById("mySquare").style.backgroundColor= "orange"
+}
+
+
+//$("#textinput").keydown(function(e) {
+//    e.keyCode; // this value
+//});
+//
+//$("#textinput").keydown(function(e) {
+//    e.keyCode; // this value
+//});
+//
+//$("#textinput").keydown(function(e) {
+//    e.keyCode; // this value
+//});
+//
+//$("#textinput").keydown(function(e) {
+//    e.keyCode; // this value
+//});
+//
+//$("#textinput").keydown(function(e) {
+//    e.keyCode; // this value
+//});
+
+
+// blue:66 green:71 orange:70 red:82 yellow: 
