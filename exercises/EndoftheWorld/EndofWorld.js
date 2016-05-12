ba6af98cac653c3e58ef431333526d729035b0b4
Megan Franco <francomegan@gmail.com>
2016-05-12 15:04:12 -0600

    updates to Business-time as well as earlier warm-ups/exercises

--- exercises/EndoftheWorld/EndofWorld.js
+++ exercises/EndoftheWorld/EndofWorld.js
@@ -1,52 +1,33 @@
 //http://www.w3schools.com/js/js_timing.asp
 //
-//have this system call a method that updates the time and the visual of it and at the end it calls itself with the updated time of course. so like call it every second,
-//    
-//up have it keep trackof the "global time" and take 1000 milliseconds off it each time it's called    
-//
-//
+//update the time in the HTML, and at the end call the function with the updated time (so call it every second,)  
+// take 1000 milliseconds off it each time it's called 
 //when it reaches zero show the popup
 //
-//do the "time math" to do more complex times (which is just for the visual they can stay in milliseconds for the system to work properly) and you have to figure out minutes and hours, 
+//figure out minutes and hours, ?
 
 
-function startTime() {
-    var today = new Date();
-    var h = today.getHours();
-    var m = today.getMinutes();
-    var s = today.getSeconds();
-    m = checkTime(m);
-    s = checkTime(s);
-    document.getElementById('countdown').innerHTML =
-    h + ":" + m + ":" + s;
-    var t = setTimeout(startTime, 500);
-}
-function checkTime(i) {
-    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
-    return i;
+var time = document.getElementById("countdown")
+
+function showTime() {
+    setInterval(function(time){
+    time = time - 1;
+    }, 1000)
 }
 
+document.getElementById("countdown").innerHTML.value = showTime();
 
 
-
-
-  var seconds;
-  var temp;
- 
-  function countdown() {
-    seconds = document.getElementById('countdown').innerHTML.toLocaleString();
-    seconds = parseInt(seconds, 10);
- 
-    if (seconds == 1) {
-      temp = document.getElementById('countdown');
-      temp.innerHTML = "The end of the world has come upon us";
-      return;
-    }
- 
-    seconds--;
-    temp = document.getElementById('countdown');
-    temp.innerHTML = seconds;
-    theEnd = setTimeout(countdown, 1000);
-  } 
- 
-  countdown();
+/////////////////// ATTEMPT 1 //////////////////////////
+//var toDisplay = setInterval(countdown(document.getElementById("countdown").innerHTML), 1000)
+//var myCountdown = function {
+//    var startAt = Number(document.getElementById("countdown"));
+//        while(startAt > 0) {
+//        //redefine startAt
+//        startAt = startAt - 1;
+//        
+//        myCountdown()    
+//        } else {/*popup*/}
+//}
+//
+//document.getElementById("countdown").innerHTML = toDisplay
