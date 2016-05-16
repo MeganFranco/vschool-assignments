283ca501c22c93085b960647c4536de15fb1c88f
Megan Franco <francomegan@gmail.com>
2016-05-15 19:36:02 -0600

    added CSS and worked on the JS/HTML forms

---
+++ projects/High-Score-Haven/scratch.js
@@ -0,0 +1,24 @@
+
+
+
+// var submitTwo = function(){
+//      //use .value to pull from forms
+//     
+//      var name = document.getElementById("userName").value;
+//      var game = document.getElementById("userGame").value;
+//      var date = document.getElementById("userDate").value;
+//      var score = document.getElementById("userScore").value;
+//	  
+//
+//      
+//      document.getElementById("newName").innerHTML = name;
+//      document.getElementById("newGame").innerHTML = game;
+//      document.getElementById("newDate").innerHTML = date;
+//      document.getElementById("newScore").innerHTML = score;
+//      
+//      	  console.log("name: " + name);
+//     
+// }
+// 
+//  document.getElementById("formSubmitTwo").onsubmit = submitTwo()
+      
