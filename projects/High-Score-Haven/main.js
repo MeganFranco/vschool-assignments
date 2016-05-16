72652975be2abfc0ab92c725b95f384a0ac3c5d8
Megan Franco <francomegan@gmail.com>
2016-05-16 12:19:55 -0600

    updates to high-score haven

--- projects/High-Score-Haven/main.js
+++ projects/High-Score-Haven/main.js
@@ -5,17 +5,16 @@
 // }
 
 //add a message box if "message" == true
-function preparePage()
-{
-     var checkBox = function(){
-     if (document.getElementById("messageOption").checked){
-         document.getElementById("commentBox").style.display = "block";
-     } else {
-        document.getElementById("commentBox").style.display = "none";
-     }
- }
+function preparePage() {
+    var checkBox = function () {
+        if (document.getElementById("messageOption").checked) {
+            document.getElementById("commentBox").style.display = "block";
+        } else {
+            document.getElementById("commentBox").style.display = "none";
+        }
+    }
 
- document.getElementById("messageOption").onlick = checkBox();
+    document.getElementById("messageOption").onlick = checkBox();
 }
 
 
@@ -29,20 +28,22 @@ function preparePage()
 //}
 
 
-window.onload = function() {
-    preparePage();
+window.onload = function () {
+//    preparePage();
+    console.log("I'm hereeee")
 }
 
- 
- 
- 
- 
- ///OPTION 1///////////
- 
-// why doesn't statement (from Lynda tutorial) below work?!?!
-//document.getElementById("formSubmit").addEventListener('submit', addRow);
 
-//stack overflow response http://stackoverflow.com/questions/26107125/cannot-read-property-addeventlistener-of-null (still not working)
+
+
+
+///OPTION 1///////////
+
+            // why doesn't statement (from Lynda tutorial) below work?!?!
+//document.getElementById("formSubmit").addEventListener('submit', addRow);
+console.log(document.getElementById("formSubmit"))
+
+//stack overflow response http:stackoverflow.com/questions/26107125/cannot-read-property-addeventlistener-of-null (still not working)
 
 //var grabForm = document.getElementById("formSubmit")
 //    if(grabForm) {
@@ -62,12 +63,12 @@ window.onload = function() {
 //    
 //    console.log("hey, I'm working");   
 //}
-
+//
 
 
 ///////////Option 2///////////////////
 //  document.getElementById("formSubmitTwo").onsubmit = submitTwo()	
- 
+
 // var submitTwo = function(){
 //      //use .value to pull from forms
 //    
@@ -88,31 +89,31 @@ window.onload = function() {
 //      document.getElementById("newScore").value = score;
 //      
 //      	  console.log("name: " + name);
-     
-     
-           //Nope...
+
+
+//Nope...
 //      document.getElementById("newName").innerHTML = name;
 //      document.getElementById("newGame").innerHTML = game;
 //      document.getElementById("newDate").innerHTML = date;
 //      document.getElementById("newScore").innerHTML = score;
-     //also Nope...
+//also Nope...
 //    var name = document.forms[0].elements[0].value;
-     
- //}
+
+//}
 //
 // document.getElementById("formSubmitTwo").onsubmit = submitTwo()	
 
- 
+
 
 // on submit -> add a new table row filled with the user information
 
 //
 
 //use a class?
-    // function userScore {
-    //
-    // }
-    
+// function userScore {
+//
+// }
+
 //error messages
 
 
