b19d1a50b99f47980a944748bf48bf5fb7bb820a
Megan Franco <francomegan@gmail.com>
2016-06-01 14:40:55 -0600

    brackets necessitated back up

--- projects/MakeYourOwnProject/app.js
+++ projects/MakeYourOwnProject/app.js
@@ -1,1 +1,15 @@
 //API key: eecbf01a57664fff99bbd3f17275ffcc
+
+var app = angular.module("GetWeather", ["ngRoute"])
+
+app.config(function ($routeProvider){
+    $routeProvider
+    .when ("/",{
+        templateUrl: "home/home.html",
+        controller: "HomeController",
+    })
+    .when ("/location", {
+        templateUrl: "userinfo/userinfo.html",
+        controller: "userinfo.js",
+    })
+})
