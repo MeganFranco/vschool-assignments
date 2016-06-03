7fd4157854f370b3603f7e9592218dd421c5cd20
Megan Franco <francomegan@gmail.com>
2016-06-03 16:12:26 -0600

    updates to brony app

--- projects/My-Little-Brony/app.js
+++ projects/My-Little-Brony/app.js
@@ -1,6 +1,12 @@
 var app = ("MyLittleBronyApp", ["ngRoute"])
 
-app.controller("MainController", [$scope, ])
+app.controller("MainController", ["$scope", "ManagerService", function($scope, ManagerService){
+    
+    $scope.addPony = function(){
+        ManagerService.addPony($pony)
+    }
+    
+}])
 
 //1 call add function $scope.addpony()
 //call managerService add pony
