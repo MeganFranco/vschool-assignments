70879081943a559487a8e122ffb0a36457da2da8
Megan Franco <francomegan@gmail.com>
2016-06-05 17:11:57 -0600

    updates to brony app

--- projects/My-Little-Brony/app.js
+++ projects/My-Little-Brony/app.js
@@ -3,34 +3,53 @@ var app = angular.module("MyLittleBronyApp", []);
 app.controller("MainController", ["$scope", "PonyService", function ($scope, PonyService) {
 
     
+    $scope.favoritePonies = PonyService.poniesList
+   // console.log($scope.favoritePonies)
+    
     
 //////////////ADD/////////////////
-    $scope.favoritePonies = PonyService.poniesList
-    console.log($scope.favoritePonies)
     
     $scope.addPony = function (pony) {
         console.log(pony);
-        PonyService.addPony(pony);
-        PonyService.addPonyToServer(pony);
-       //NOT DISPLAYING WHEN ONLY ADDING TO SERVER
+//        PonyService.addPony(pony);
+       PonyService.addPonyToServer(pony);
+       
+        
+        $scope.pony = {};
     };
 ///////////////SHOW///////////////
     $scope.showPonies = function (){
+    PonyService.showAllPonies();
+        console.log($scope.favoritePonies)
         
-        $scope.favoritePonies = PonyService.takeFromServer();
         
     }
     
 //////////////DELETE//////////////////////    
-    $scope.deletePony = function($index){
-        //PonyService.deletePony($index);
-        PonyService.deletePonyFromServer($index);
+    $scope.deletePony = function(pony){
+        PonyService.deletePony(pony);
+        PonyService.deletePonyFromServer(pony);
+//        PonyService.showAllPonies();
     }
     
-        
-}]);
 
-////////////////////////////
+    
+    
+///////////EDIT/////////////////
+    $scope.editPony = function(pony, index){
+        
+        $scope.favoritePonies = PonyService.editPonyFn(pony, index); 
+        console.log("hello")
+
+        //let them change
+        //make new pony
+        //save new pony
+        //delete old pony
+    }
+    
+    
+    
+}]);
 
 
 
--- projects/My-Little-Brony/services.js
+++ projects/My-Little-Brony/services.js
@@ -1,9 +1,18 @@
 var app = angular.module("MyLittleBronyApp");
 
-app.service("PonyService", ["$http", function ($http){
+app.service("PonyService", ["$http", function ($http) {
     var self = this;
 
-    this.poniesList = [];
+    this.poniesList = [
+//        {
+//            name: "Twilight Sparkle",
+//            imgUrl: "http://pre02.deviantart.net/8872/th/pre/i/2012/206/7/d/twilight_sparkle_by_hankofficer-d46dfaw.png"
+//    },
+//        {
+//            name: "Applejack",
+//            imgUrl: "http://vignette4.wikia.nocookie.net/mlpfanart/images/f/ff/Applejack_3_by_xpesifeindx-d5gsde5.png/revision/latest?cb=20131015231355"
+//    },
+    ];
 
 
 
@@ -13,7 +22,7 @@ app.service("PonyService", ["$http", function ($http){
         var response = ponyValidator(newPony)
         if (response.success) {
             this.poniesList.push(newPony);
-//            console.log(this.poniesList)
+            //            console.log(this.poniesList)
         } else {
             return response.message;
             //            throw new Error("Pony Not Valid")
@@ -26,58 +35,79 @@ app.service("PonyService", ["$http", function ($http){
     }
 
     ///////////////get list of ponies////////////////    
-    this.getPonyList = function (arrayOfPonies) {
-        return ModelService.poniesList;
-    }
+    //    this.getPonyList = function (arrayOfPonies) {
+    //        return poniesList;
+    //    }
 
     ////////////////Check Submission//////////////////  
     var ponyValidator = function (newPony) {
-        if (newPony.name.length <= 3) {
-            return {
-                message: "Pony name must be longer than 3 characters",
-                success: false
+            if (newPony.name.length <= 3) {
+                return {
+                    message: "Pony name must be longer than 3 characters",
+                    success: false
+                }
+            } else {
+                return {
+                    success: true
+                }
             }
-        } else {
-            return {
-                success: true
-            }
+
         }
+        //==================SERVER STUFF========================//
 
-    }
-
-
-    ///////////////Get from Server//////////////////
-    this.takeFromServer = function () {
-//        console.log(self.poniesList)
-          return $http.get("http://mean.codingcamp.us/meganfranco/pony")
-                .then(function (response) {
-                        self.poniesList = response.data;
-                        console.log(self.poniesList)
-
-                    })
-//        console.log(this.poniesList)
-    }
     ///////////////////Add to Server///////////////////////////
-    this.addPonyToServer = function(pony) {
+    this.addPonyToServer = function (pony) {
         $http.post("http://mean.codingcamp.us/meganfranco/pony", pony)
-        .then(function(response) {
+            .then(function (response) {
                 self.poniesList.push(response.data);
-                
-                console.log(self.poniesList)
-                
+                console.log(response.data)
+
             })
     };
-    
+
+
     ////////////////////Delete from server///////////////////////////
+
+    this.deletePonyFromServer = function (pony) {
+        console.log(pony);
+        $http.delete("http://mean.codingcamp.us/meganfranco/pony/" + pony._id)
+            .then(function (response) {
+//                console.log(response.data)
+                    ///splice will start removing at the first parameter and then it removes the amount of items specified in the second parameter 
+                  
+            });
+
+
+    }
+
+
+
+    /////////////////Get from Server////////////////////
+    this.showAllPonies = function () {
+           
+        return $http.get("http://mean.codingcamp.us/meganfranco/pony")
+            .then(function (response) {
+                for (var i = 0; i < response.data.length; i++) {
+                    if(self.poniesList.indexOf(response.data[i] == -1))
+                    {self.poniesList.push(response.data[i]);}
+                }
+
+                console.log(response.data)
+
+            })
+    }
     
-    this.deletePonyFromServer = function(index){
-        $http.delete("http://mean.codingcamp.us/meganfranco/pony", index)
-        console.log(index)
-        .then(function(response){
-            self.poniesList.splice(response.data, 1);
+    
+//////////////edit item//////////////////
+    
+    this.editPonyFn = function (pony, index){
+        $http.put("http://mean.codingcamp.us/megan/todo/" + pony._id, pony)
+            .then(function(response){
             
-            console.log("here!")
-        })
+            self.poniesList[index] = response.data;
+            
+             pony.editing = !pony.editing
+        });
     }
 
 }]);
