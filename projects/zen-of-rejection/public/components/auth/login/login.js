ac20a435c5b7edc7751546036f9dca0c879e6b1e
Megan Franco <francomegan@gmail.com>
2016-07-21 14:21:12 -0600

    fixed some content

--- projects/zen-of-rejection/public/components/auth/login/login.js
+++ projects/zen-of-rejection/public/components/auth/login/login.js
@@ -3,7 +3,7 @@ var app = angular.module("RejectionApp.Auth");
 app.controller("LoginController", ["$scope", "$location", "UserService", function ($scope, $location, UserService) {
     $scope.login = function (user) {
         UserService.login(user).then(function(response) {
-            $location.path("/responses");
+            $location.path("/");
         }, function(response) {
             alert("There was a problem: " + response.data.message);
         });
